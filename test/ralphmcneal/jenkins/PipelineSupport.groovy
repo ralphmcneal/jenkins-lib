0d0ade1da84b22e4ef78f259c1f2439726212aa2
ralphmcneal <ralph.mcneal@centurylink.com>
2017-11-17 11:24:10 -0600

    allow multi args and closures for verification

--- test/ralphmcneal/jenkins/PipelineSupport.groovy
+++ test/ralphmcneal/jenkins/PipelineSupport.groovy
@@ -1,12 +1,13 @@
 package ralphmcneal.jenkins
 
 import com.lesfurets.jenkins.unit.BasePipelineTest
+import com.lesfurets.jenkins.unit.MethodCall
 import org.junit.Before
 
 import static junit.framework.TestCase.assertEquals
 import static org.junit.Assert.assertTrue
 
-public class PipelineSupport extends BasePipelineTest {
+class PipelineSupport extends BasePipelineTest {
     @Before
     void setUp() throws Exception {
         super.setUp()
@@ -16,15 +17,35 @@ public class PipelineSupport extends BasePipelineTest {
         verify(1, method, args)
     }
 
-    void verify(int count, String method, String args) {
+    void verify(int count, String method, String... args) {
         def methodCalls = helper.callStack.findAll { call ->
-                call.methodName == method
+            call.methodName == method
         }
 
-        assertTrue("Expected invocations of \'$method\' are incorrect. Wanted $count but received ${methodCalls?.size()}", methodCalls?.size() == count)
+        verifyCallCount(count, method, methodCalls)
 
-        methodCalls.each { call ->
-                assertEquals("Expected args are incorrect", args, call.argsToString())
+        methodCalls.eachWithIndex { call, i ->
+            assertEquals("Expected args do not match", (args.size() > 1 ? args[i] : args[0]), call.argsToString())
         }
+    }
+
+    void verify(String method, Closure<Boolean>... verifier) {
+        verify(1, method, verifier)
+    }
+
+    void verify(int count, String method, Closure<Boolean>... verifier) {
+        def methodCalls = helper.callStack.findAll { call ->
+            call.methodName == method
+        }
+
+        verifyCallCount(count, method, methodCalls)
+
+        methodCalls.eachWithIndex { call, i  ->
+            assertTrue('Invocation $i of $method failed verification', (verifier.size() > 1 ? verifier[i](call.args) : verifier[0](call.args)))
+        }
+    }
+
+    private verifyCallCount(int expectedCount, String method, List<MethodCall> methodCalls) {
+        assertTrue("Expected invocations of \'$method\' are incorrect. Wanted $expectedCount but received ${methodCalls?.size()}", methodCalls?.size() == expectedCount)
     }
 }
